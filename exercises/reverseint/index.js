3203bde6bc905f9f993a973080f19cadc7e3d5a6
shanelonergan <sptlonergan@gmail.com>
2020-01-20 20:29:15 -0500

    working on reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const  stringInt = n.toString('')
+
+    const arr = stringInt.split('')
+}
 
 module.exports = reverseInt;

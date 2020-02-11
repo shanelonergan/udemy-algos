4edbb296f1ecfe19546860279409a838f68db89b
shanelonergan <sptlonergan@gmail.com>
2020-02-11 11:10:56 -0500

    second soln

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -20,4 +20,16 @@ function capitalize(str) {
     return output.join(' ')
 }
 
+function capitalize2(str) {
+    let result = str[0].toUpperCase()
+
+    for(let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase()
+        } else {
+            result += str[i]
+        }
+    }
+}
+
 module.exports = capitalize;

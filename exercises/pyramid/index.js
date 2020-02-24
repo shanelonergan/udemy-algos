6a0a7dcccbaf519bc41a91f5afd1ac95816d735c
shanelonergan <sptlonergan@gmail.com>
2020-02-24 10:33:16 -0500

    second solb

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -38,4 +38,20 @@ function pyramid(n) {
     }
 }
 
+function pyramid2(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2)
+
+    for (let row = 0; row < n; row++) {
+        let level = ''
+
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if (midpoint - row <= column && midpoint + row >= column) {
+                level += '#'
+            } else {
+                level += ' '
+            }
+        }
+    }
+}
+
 module.exports = pyramid;

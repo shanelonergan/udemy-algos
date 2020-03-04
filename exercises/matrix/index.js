bb60ce7575192835ae9b7ae975f952f5ee8f2a03
shanelonergan <sptlonergan@gmail.com>
2020-03-04 12:42:10 -0500

    working soln

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -16,7 +16,57 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
+    const results = []
+
+    for (let i = 0; i < n; i++) {
+        results.push([])
+    }
+
     let counter = 1
+
+    let startColumn = 0
+    let endColumn = n - 1
+    let startRow = 0
+    let endRow = n - 1
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+
+        // top row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter
+            counter++
+        }
+        startRow++
+
+        // right column
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = counter
+            counter++
+        }
+        endColumn--
+
+        // bottom row
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter
+            counter ++
+        }
+        endRow--
+
+        // start column
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter
+            counter++
+        }
+        startColumn++
+
+    }
+
+    console.log(results)
+    return results
+}
+
+function attemptedMatrix(n) {
+        let counter = 1
     let results = []
 
     for (let i = 1; i < n; i++) {
@@ -64,8 +114,6 @@ function matrix(n) {
     //         console.log(array)
     //     });
     // }
-    console.log(results)
-    return results
 }
 
 module.exports = matrix;

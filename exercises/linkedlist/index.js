7632a95be884d4f71fef424baa6a4c54cb0ac76c
shanelonergan <sptlonergan@gmail.com>
2020-04-01 16:42:58 -0400

    getFirst and getLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -28,19 +28,45 @@ class LinkedList {
     }
 
     size() {
-        if(!this.head) {
-            return 0
-        }
+        // // My soln
+        // if(!this.head) {
+        //     return 0
+        // }
 
-        let counter = 1
-        let currentNode = this.head
+        // let counter = 1
+        // let currentNode = this.head
 
-        while(currentNode.next) {
-            counter += 1
-            currentNode = currentNode.next
+        // while(currentNode.next) {
+        //     counter++
+        //     currentNode = currentNode.next
+        // }
+
+        // return counter
+
+        // Course Soln
+        let counter = 0
+        let node = this.head
+
+        while(node) {
+            counter++
+            node = node.next
         }
 
         return counter
+    }
+
+    getFirst() {
+        return this.head
+    }
+
+    getLast() {
+        let node = this.head
+
+        while(node.next) {
+            node = node.next
+        }
+
+        return node
     }
 }
 

7dbf4af3f03c3cff6165e01d67dbde4e51119e84
shanelonergan <sptlonergan@gmail.com>
2020-04-10 10:21:40 -0400

    events done

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,18 +4,29 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {}
+  }
   // Register an event handler
   on(eventName, callback) {
-
+    if (!this.events[eventName]) {
+      this.events[eventName] = [callback]
+    } else {
+      this.events[eventName].push(callback)
+    }
   }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    this.events[eventName].forEach(event => event.call())
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.events[eventName] = []
+  }
 }
 
 module.exports = Events;

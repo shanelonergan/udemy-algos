58c409924b5684a96bed0f1cdcd9d501d158950a
shanelonergan <sptlonergan@gmail.com>
2020-04-13 10:19:01 -0400

    bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,19 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            const firstElement = arr[j]
+            const secondElement = arr[j + 1]
 
+            if(firstElement > secondElement) {
+                arr[j] = secondElement
+                arr[j + 1] = firstElement
+            }
+        }
+    }
+
+    return arr
 }
 
 function selectionSort(arr) {

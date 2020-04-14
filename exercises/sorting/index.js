4ae0117a7c4a5eea093e3c450fc4fe6de300f594
shanelonergan <sptlonergan@gmail.com>
2020-04-14 11:24:07 -0400

    merge sort working

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -40,7 +40,15 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if(arr.length === 1) {
+        return arr
+    }
 
+    const midpoint = Math.floor(arr.length / 2)
+    const left = arr.slice(0, midpoint)
+    const right = arr.slice(midpoint)
+
+    return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
